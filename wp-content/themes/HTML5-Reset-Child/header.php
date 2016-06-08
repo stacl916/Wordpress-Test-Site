1c1948107183a1204b30bf8dd52ff52b8e197124
stacl916 <stacl916@hsd.k12.or.us>
2016-06-07 20:11:50 -0700

    Update header.php

--- wp-content/themes/HTML5-Reset-Child/header.php
+++ wp-content/themes/HTML5-Reset-Child/header.php
@@ -129,5 +129,7 @@
 		</header>
 
 		<nav id="nav" role="navigation">
-			<?php wp_nav_menu( array('theme_location' => 'primary') ); ?>
+			
+			<?php wp_nav_menu( array('theme_location' => 'primary', 'menu_class' => 'nav nav-tabs', 'container' => 'ul' ) ); ?>
+			
 		</nav>
